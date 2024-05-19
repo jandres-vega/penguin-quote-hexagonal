74d33b9dcceeea6260cbee2821a065a473776c20
jandres-vega <jandresvega@itsa.edu.co>
2024-05-18 23:31:41 -0500

    Init project

---
+++ src/main/java/com/library/PenguinQuoteHexagonalApplication.java
@@ -0,0 +1,13 @@
+package com.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PenguinQuoteHexagonalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PenguinQuoteHexagonalApplication.class, args);
+    }
+
+}
